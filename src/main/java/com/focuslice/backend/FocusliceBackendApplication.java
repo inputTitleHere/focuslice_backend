2ef60090a937a8170678402b0f101003b1ec9a0f
InputTitleHere <seanbryan97@naver.com>
2026-01-11 13:37:50 +0900

    init: spring boot init

---
+++ src/main/java/com/focuslice/backend/FocusliceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.focuslice.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusliceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusliceBackendApplication.class, args);
+	}
+
+}
